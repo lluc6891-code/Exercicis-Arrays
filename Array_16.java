1b1ce3618cc74234828587c5b1562eb3d7008ae3
Lluc <lluc6891@iessineu.net>
2026-05-13 11:58:37 +0200

    Acabat array 16 i 17

--- Array_16.java
+++ Array_16.java
@@ -1,7 +1,26 @@
 
+import java.util.Arrays;
+
 public class Array_16 {
 
     public static void main(String[] args) {
 
+        final int tamany = 5;
+
+        int num[] = new int[tamany];
+
+        num[0] = 6;
+        num[1] = 3;
+        num[2] = 9;
+        num[3] = 4;
+        num[4] = 1;
+
+        Arrays.sort(num);
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
     }
+
 }
--- Array_17.java
+++ Array_17.java
@@ -3,5 +3,38 @@ public class Array_17 {
 
     public static void main(String[] args) {
 
+        int numeros[] = {5, 6, 2, 10, 1};
+
+        ordenacioBimbolla(numeros);
+
+        //mostrar el contenido
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
     }
+
+    /**
+     * @param lista
+     *
+     */
+    public static void ordenacioBimbolla(int lista[]) {
+        int cuentaintercambios = 0;
+        for (boolean ordenado = false; !ordenado;) {
+            for (int i = 0; i < lista.length - 1; i++) {
+                if (lista[i] > lista[i + 1]) {
+                    int variableauxiliar = lista[i];
+                    lista[i] = lista[i + 1];
+                    lista[i + 1] = variableauxiliar;
+                    cuentaintercambios++;
+                }
+            }
+
+            if (cuentaintercambios == 0) {
+                ordenado = true;
+            }
+            cuentaintercambios = 0;
+        }
+    }
+
 }
